2526260d38aa1e0c8f1719ad4d02ca6981450b80
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-05-20 17:39:09 +0800

    Create freshnews.ts

---
+++ src/routes/freshnews.ts
@@ -0,0 +1,35 @@
+import type { RouterData } from "../types.js";
+import type { RouterType } from "../router.types.js";
+import { get } from "../utils/getData.js";
+import { getTime } from "../utils/getTime.js";
+
+export const handleRoute = async (_: undefined, noCache: boolean) => {
+  const listData = await getList(noCache);
+  const routeData: RouterData = {
+    name: "freshnews",
+    title: "Fresh News",
+    type: "推荐榜",
+    link: "https://www.freshnewsasia.com/index.php/en/",
+    total: listData.data?.length || 0,
+    ...listData,
+  };
+  return routeData;
+};
+
+const getList = async (noCache: boolean) => {
+  const url = `https://dev.nexthubs.fun/webhook/freshnewsapi`;
+  const result = await get({ url, noCache });
+  const list = result.data[0].list;
+  return {
+    ...result,
+    data: list.map((v: RouterType["freshnews"]) => ({
+      id: v.docid,
+      title: v.title,
+      cover: undefined,
+      author: v.type,
+      hot: undefined,
+      url: `https://www.freshnewsasia.com/${v.docid}`,
+      mobileUrl: `https://www.freshnewsasia.com/${v.docid}`,
+    })),
+  };
+};
