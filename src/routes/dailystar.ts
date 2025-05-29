d17db52fe6dd117c1eb564d12fec6435826ade28
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2025-05-29 20:21:42 +0800

    Create dailystar.ts

---
+++ src/routes/dailystar.ts
@@ -0,0 +1,32 @@
+import type { RouterData } from "../types.js";
+import type { RouterType } from "../router.types.js";
+import { get } from "../utils/getData.js";
+import { getTime } from "../utils/getTime.js";
+
+export const handleRoute = async (_: undefined, noCache: boolean) => {
+  const listData = await getList(noCache);
+  const routeData: RouterData = {
+    name: "dailystar",
+    title: "The Daily Star",
+    type: "热门榜",
+    link: "https://www.thedailystar.net",
+    total: listData.data?.length || 0,
+    ...listData,
+  };
+  return routeData;
+};
+
+const getList = async (noCache: boolean) => {
+  const url = `http://n8n.giaogiao.work/webhook/dailystar`;
+  const result = await get({ url, noCache });
+  const list = result.data[0].list;
+  return {
+    ...result,
+    data: list.map((v: RouterType["dailystar"]) => ({
+      id: v.docid,
+      title: v.title,
+      url: `https://www.thedailystar.net/${v.docid}`,
+      mobileUrl: `https://www.thedailystar.net/${v.docid}`,
+    })),
+  };
+};
