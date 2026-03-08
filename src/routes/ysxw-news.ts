2b98f0a13a2ec03f2d29efad53baecc5d76876cd
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-03-08 21:27:23 +0800

    Update webhook URL for ysxw news route

--- src/routes/ysxw-news.ts
+++ src/routes/ysxw-news.ts
@@ -17,7 +17,7 @@ export const handleRoute = async (_: undefined, noCache: boolean) => {
 };
 
 const getList = async (noCache: boolean) => {
-  const url = `https://dev.nexthubs.fun/webhook/ysxw`;
+  const url = `https://n8n.nmco.work/webhook/ysxw`;
   const result = await get({ url, noCache });
   const list = result.data[0].list;
   return {
