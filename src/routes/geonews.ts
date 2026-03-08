12a5532c4c21a0cb3fbf28d3f854fe6e6a8a7511
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-03-08 21:24:06 +0800

    Update geonews API endpoint URL

--- src/routes/geonews.ts
+++ src/routes/geonews.ts
@@ -17,7 +17,7 @@ export const handleRoute = async (_: undefined, noCache: boolean) => {
 };
 
 const getList = async (noCache: boolean) => {
-  const url = `https://dev.nexthubs.fun/webhook/geonewsapi`;
+  const url = `https://n8n.nmco.work/webhook/geonewsapi`;
   const result = await get({ url, noCache });
   const list = result.data[0].list;
   return {
