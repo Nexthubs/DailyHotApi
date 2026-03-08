e68ed4687000d70f9c4790528859ee2e35cf8b0d
我是找找 <98395115+Nexthubs@users.noreply.github.com>
2026-03-08 21:25:02 +0800

    Update webhook URL for vnexpress route

--- src/routes/vnexpress.ts
+++ src/routes/vnexpress.ts
@@ -17,7 +17,7 @@ export const handleRoute = async (_: undefined, noCache: boolean) => {
 };
 
 const getList = async (noCache: boolean) => {
-  const url = `https://dev.nexthubs.fun/webhook/vnexpress`;
+  const url = `https://n8n.nmco.work/webhook/vnexpress`;
   const result = await get({ url, noCache });
   const list = result.data[0].list;
   return {
